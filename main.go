f57ef07a984b358d8f2e48c13298ea7801d035f5
Tomas Kozak <kozak@talko.cz>
2023-11-29 13:21:05 +0100

    Check Github with every request

--- main.go
+++ main.go
@@ -17,17 +17,22 @@ func init() {
 func main() {
 	flag.Parse()
 
-	components := GetGithubStatusComponents()
 	monitor := NewMonitor()
 
-	for _, component := range components {
-		if component.Status == "operational" {
-			monitor.GithubComponentStatus.WithLabelValues(component.Name, component.Status).Set(1)
-		} else {
-			monitor.GithubComponentStatus.WithLabelValues(component.Name).Set(0)
-		}
+	middleware := func(handlerFor http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			components := GetGithubStatusComponents()
+			for _, component := range components {
+				if component.Status == "operational" {
+					monitor.GithubComponentStatus.WithLabelValues(component.Name, component.Status).Set(1)
+				} else {
+					monitor.GithubComponentStatus.WithLabelValues(component.Name).Set(0)
+				}
+			}
+			handlerFor.ServeHTTP(w, r)
+		})
 	}
 
-	http.Handle("/metrics", promhttp.HandlerFor(monitor.Registry, promhttp.HandlerOpts{Registry: monitor.Registry}))
+	http.Handle("/metrics", middleware(promhttp.HandlerFor(monitor.Registry, promhttp.HandlerOpts{Registry: monitor.Registry})))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", ePort), nil))
 }
